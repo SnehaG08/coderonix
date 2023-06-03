8beca137829991ea540403ff65643d5e87630046
Sneha Gupta <snehaguptaprg@gmail.com>
2023-06-03 12:47:54 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Coderonix - Code made easy </title>
+    <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+    
+    <div class="main">
+
+        <div class="midtxt">
+            Coderonix
+        </div>
+
+        <a href="#browse">
+            <div class="indicator">
+                <span></span>
+                <span></span>
+            </div>  
+        </a>
+        
+
+        <!--<div class="nav">
+            <div class="logo"> Coderonix </div>
+            <div class="tabs">
+                <ul>
+                    <li><a href=""> View Courses </a></li>
+                    <li><a href=""> About Us </a></li>
+                </ul>
+            </div>
+        </div>
+        
+        <div class="main-2">
+            
+            <div class="box-2 creator">
+                <div class="bhead"> I am a course creator </div>
+                <a href=""><button class="btn btnlogin"> Login\SignUp </button></a>
+            </div>
+
+            <div class="box-2 learner">
+                <div class="bhead"> I am a learner </div>
+                <a href=""></a><button class="btn btnlogin"> Login\SignUp </button></a>
+            </div>
+
+        </div>-->
+    </div>
+
+    <div class="browse" id="browse">
+        <div class="brhead">
+            Browse Categories
+        </div>
+        <div class="brdata">
+            <div class="box-4 box1">
+                <div class="bhead">Cat1</div>
+            </div>
+
+            <div class="box-4 box2">
+                <div class="bhead">Cat2</div>
+            </div>
+
+            <div class="box-4 box3">
+                <div class="bhead">Cat3</div>
+            </div>
+
+            <div class="box-4 box4">
+                <div class="bhead">Cat4 </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
