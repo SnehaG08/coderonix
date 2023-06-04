2db544c4b1daa40a65f22fe7263c41709dff16d0
Sneha Gupta <snehaguptaprg@gmail.com>
2023-06-04 11:50:27 +0530

    changed

--- index.php
+++ index.php
@@ -11,6 +11,13 @@
     
     <div class="main">
 
+        <div class="nav">
+            <ul>
+                <li><a href="login.php">Login</a></li>
+                <li><a href="signup.php">SignUp</a></li>
+            </ul>
+        </div>
+
         <div class="midtxt">
             Coderonix
         </div>
